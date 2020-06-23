c8c09351ff2b919460f8dc08d16ba796ebc8b78f
Masahiro Okubo <stmhamachiii@gmail.com>
2020-06-23 12:55:17 +0900

    eventsのspec作成

--- spec/helpers/authorization_helper.rb
+++ spec/helpers/authorization_helper.rb
@@ -1,7 +1,6 @@
 module AuthorizationHelper
   def sign_up(user)
-
-    # The argument 'user' should be a hash that includes the params 'email' and 'password'.
+    # Authのリンクが変わる場合は修正が必要
     post '/api/auth',
          # factorybotのpasswordからデータをとるとエラーが出るので手入力で対応
          params: {
@@ -14,14 +13,10 @@ module AuthorizationHelper
   end
 
   def auth_tokens_for_user(user)
-    puts 'user data--------'
-    puts user
-    puts 'user data--------'
-    # The argument 'user' should be a hash that includes the params 'email' and 'password'.
+    # Authのリンクが変わる場合は修正が必要
     post '/api/auth/sign_in/',
          params: { email: user['email'], password: '11111111' },
          as: :json
-    # The three categories below are the ones you need as authentication headers.
     response.headers.slice('client', 'access-token', 'uid')
   end
 end
--- spec/requests/api/events_spec.rb
+++ spec/requests/api/events_spec.rb
@@ -4,47 +4,42 @@ require 'rails_helper'
 
 
 RSpec.describe Api::EventsController, type: :request do
-  describe 'GET /api/events' do
-    context 'when authorization is valid' do
-      it 'returns events' do
-        user = build :user
-        sign_up(user)
-        @tokens = auth_tokens_for_user(user)
 
-        FactoryBot.create_list(:event, 10)
-        get '/api/events', headers: @tokens
+  before do
+    user = build :user
+    sign_up(user)
+    @tokens = auth_tokens_for_user(user)
+  end
 
-        json = JSON.parse(response.body)
-        expect(json).not_to be_empty
-        expect(json['response'].length).to eq(10)
-      end
+  context 'when authorization is valid' do
+    it 'returns events' do
+      FactoryBot.create_list(:event, 10)
+      get '/api/events', headers: @tokens
 
-      # it 'returns status code 200' do
-      #   expect(response).to have_http_status(200)
-      # end
+      json = JSON.parse(response.body)
+      expect(json).not_to be_empty
+      expect(json['response'].length).to eq(10)
     end
 
+    it 'returns status code 200' do
+      get '/api/events', headers: @tokens
+      expect(response).to have_http_status(200)
+    end
+  end
 
-    # it 'gets all events' do
-    #   auth_tokens = sign_in(user)
-    #   p auth_tokens
-    #   FactoryBot.create_list(:event, 10)
-    #
-    #   get '/api/events'
-    #   json = JSON.parse(response.body)
-    #
-    #   # リクエスト成功を表す200が返ってきたか確認する。
-    #   expect(response.status).to eq(200)
-    #
-    #   # 正しい数のデータが返されたか確認する。
-    #   expect(json['data'].length).to eq(10)
+  context 'when authorization is invalid' do
+    it 'does not returns events' do
+      FactoryBot.create_list(:event, 10)
+      get '/api/events', headers: nil
+
+      json = JSON.parse(response.body)
+      expect(json).not_to be_empty
+      expect(json['errors']).to eq(["You need to sign in or sign up before continuing."])
     end
 
-
-  # describe 'GET /events' do
-  #   it 'works! (now write some real specs)' do
-  #     get events_path
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
+    it 'returns status code 401' do
+      get '/api/events', headers: nil
+      expect(response).to have_http_status(401)
+    end
+  end
 end
